0e6644c7da5f8c7d0fd559e8185cafab5b407a50
ttriche <tim.triche@gmail.com>
2016-01-05 20:17:26 -0800

    loosen up arguments to getSRP so it's more useful

--- R/getSRP.R
+++ R/getSRP.R
@@ -1,18 +1,19 @@
-#' get samples for an SRA series (SRP)
+#' get samples (or other stuff) for an SRA series (SRP) or other identifier
 #' 
 #' @param SRP   the series identifier
+#' @param ...   other arguments to pass to SRAdb::sraConvert
 #' 
 #' @return      the samples and some descriptions
 #'
 #' @aliases gSRP
 #' 
 #' @export
-getSRP <- function(SRP) {
+getSRP <- function(SRP, ...) {
   if (!exists("sradb")) sradb <- checkForUpdates("SRA")
   con <- dbConnect(dbDriver("SQLite"), sradb)
-  samples <- sraConvert(SRP, out_type="sample", sra_con=con)
+  res <- sraConvert(SRP, sra_con=con, ...)
   dbDisconnect(con)
-  return(samples)
+  return(res)
 }
 
 #' @describeIn getSRP shortcut for lazy typists
