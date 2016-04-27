7a97cfdf00bb0d4f940fc90a271344f5657810ee
ttriche <tim.triche@gmail.com>
2016-04-27 12:50:46 -0700

    fixed checkForUpdates() to handle new Dropbox fuckery

--- R/checkForUpdates.R
+++ R/checkForUpdates.R
@@ -25,8 +25,23 @@ checkForUpdates <- function(whatKind=c("GEO","SRA")) {
   message(paste("Checking for", whatKind, "updates..."))
   if (!file.exists(paste0(cachePath, "/", dbFile))) {
     unlink(list.files(cachePath, pattern=oldFilePattern))
+    destFile <- paste(cachePath, paste0(dbFile, ".gz"), sep="/")
+    if (whatKind == "GEO") message("Using manually updated GEOmetadb URL... !")
+  
+    # GEOmetadb getSQLfile stopped working, so here is a fix for it... 
+    GEOurl <- "https://dl.dropboxusercontent.com/u/51653511/GEOmetadb.sqlite.gz"
+    getGEOmetadb <- function(GEOurl, destfile) { # {{{
+      download.file(GEOurl, destfile=destFile)
+      gunzip(destfile, overwrite=TRUE)
+      con <- dbConnect(SQLite(), gsub("[.]gz$", "", destfile))
+      dat <- dbGetQuery(con, "select * from metaInfo")
+      dbDisconnect(con)
+      cat("Metadata associate with downloaded file:\n")
+      print(dat)
+      return(unzippedlocalfile)
+    } # }}}
     switch(whatKind,
-           GEO=getSQLiteFile(destdir=cachePath, destfile=paste0(dbFile, ".gz")),
+           GEO=getGEOmetadb(GEOurl, destfile=destFile),
            SRA=getSRAdbFile(destdir=cachePath, destfile=paste0(dbFile, ".gz")))
   }
   db <- paste(cachePath, dbFile, sep="/")
